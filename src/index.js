198362c28b123300d716a740c34757be34a0be68
Thiago Demas <thiago.demas7@gmail.com>
2020-07-30 15:19:29 -0300

    add Category page

--- src/index.js
+++ src/index.js
@@ -6,15 +6,17 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Categoria'
 
 
 
 
 ReactDOM.render(
   <BrowserRouter>
-   <Switch>     
+   <Switch>
     <Route path="/" component={Home} exact/>
     <Route path="/cadastro/video" component={CadastroVideo} />
+    <Route path="/cadastro/categoria" component={CadastroCategoria} />
    </Switch>
   </BrowserRouter>,
   document.getElementById('root')
