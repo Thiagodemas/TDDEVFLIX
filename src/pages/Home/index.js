6c8a968403ce35b78e8b163d64eed9825dfd7303
Thiago Demas <thiago.demas7@gmail.com>
2020-07-30 15:10:14 -0300

    add component PageDefault

---
+++ src/pages/Home/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import Menu from '../../components/Menu';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
+import BannerMain from '../../components/BannerMain';
+import DadosIniciais from '../../data/dados_iniciais.json';
+
+function Home() {
+  return (
+    <div style={{background: '#141414'}}>
+     <Menu />
+      <BannerMain
+        videoTitle={"O que faz uma desenvolvedora Front-end? "}
+        url={DadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end? Trabalhando na área"}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={DadosIniciais.categorias[0]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={DadosIniciais.categorias[1]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={DadosIniciais.categorias[2]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={DadosIniciais.categorias[3]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={DadosIniciais.categorias[4]}
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={DadosIniciais.categorias[5]}
+      />
+      <Footer/>
+    </div>
+  );
+}
+
+export default Home;
